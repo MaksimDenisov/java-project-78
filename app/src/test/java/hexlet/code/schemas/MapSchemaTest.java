21486226ca10bdd48e2d8bef9b09cf440cae330f
max <max.nick.demax@gmail.com>
2023-05-09 07:32:25 +0300

    Fix style bugs

--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MapSchemaTest {
+class MapSchemaTest {
     private Validator validator;
     private MapSchema schema;
 
--- app/src/test/java/hexlet/code/schemas/NestedMapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/NestedMapSchemaTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NestedMapSchemaTest {
+class NestedMapSchemaTest {
     private Validator validator;
     private MapSchema schema;
 
